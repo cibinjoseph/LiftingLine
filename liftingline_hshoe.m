54bf0b010abcd2b3f516b1cb55d64c239cd36650
cibinjoseph <cibinjoseph92@gmail.com>
2019-09-25 11:41:50 +0530

    Change induced velocity formulation

--- liftingline_hshoe.m
+++ liftingline_hshoe.m
@@ -1,26 +1,27 @@
 % Lifting Line theory (horseshoe vortices method)
 clc; clear; clf;
 
-ll_fourier
-hold on
-
 % Parameters
 span = 6;
-chord0 = 1;
-ns = 100;
+chordMax = 1.0;
+ns = 40;
 theta = 5*(pi/180);
 cA = [100 ; 0 ; 0];
 cD = [100 ; 0 ; 0];
 Vinf = [10 ; 0 ; 0];
 rho = 1.2;
+CLslope = 2*pi;
 
 % Create geometry
-thetaSpan = linspace(0.001, 0.999*pi, ns+1);
-chord = chord0*sin(thetaSpan); chord = 0.5*(chord(1:end-1)+chord(2:end));
-yvec = -0.5*span*cos(thetaSpan); 
-yElem = 0.5*(yvec(1:end-1)+yvec(2:end));
-dy = yvec(2:end)-yvec(1:end-1);
-chord_mean = sum(abs(yElem).*chord)/span;
+semiSpan = span*0.5;
+thetaSpan = linspace(0.001, 0.999*pi, ns);
+chord = chordMax*sin(thetaSpan);
+y = -semiSpan*cos(thetaSpan);
+yvec = [-semiSpan 0.5*(y(1:end-1)+y(2:end)) semiSpan];
+dy = diff([-semiSpan 0.5*(y(2:end)+y(1:end-1)) semiSpan]);
+wingArea = sum(chord.*dy);
+chordMean = wingArea/span;
+AR = span*span/wingArea;
 
 cB = zeros(3,ns); cB(1,:) = 0.25*chord;
 cC = cB;
@@ -29,11 +30,11 @@ cC(2,:) = yvec(2:end);
 
 cp = zeros(3,ns);
 cp(1,:) = 0.75*chord;
-cp(2,:) = yElem;
+cp(2,:) = y;
 
 indVelcp = zeros(3,ns);
 indVelcp(1,:) = 0.25*chord;
-indVelcp(2,:) = yElem;
+indVelcp(2,:) = y;
 
 ncap = [sin(theta); 0; cos(theta)];
 
@@ -55,12 +56,6 @@ end
 RHS = ones(ns,1)*dot(-Vinf,ncap);
 gam = AIC\RHS;
 
-% Lift from gam
-lift_gam = rho*norm(Vinf)*gam.*dy;
-cl_gam = lift_gam/(0.5*rho*(norm(Vinf))^2*span*chord_mean);
-
-% ==================================================================
-
 % Calculate induced velocity
 indVel = zeros(1,ns);
 for is = 1:ns
@@ -69,21 +64,52 @@ for is = 1:ns
     cD(2) = cC(2,ipanel);
     indVel(is) = indVel(is) + dot(vortxl(indVelcp(:,is),cA,cB(:,ipanel),gam(ipanel)),[0;0;1]);
     indVel(is) = indVel(is) + dot(vortxl(indVelcp(:,is),cC(:,ipanel),cD,gam(ipanel)),[0;0;1]);
+    if (is ~= ipanel)
+      indVel(is) = indVel(is) + dot(vortxl(indVelcp(:,is),cB(:,ipanel),cC(:,ipanel),gam(ipanel)),[0;0;1]);
+    end
   end
 end
+
+% Lift from gam
+lift_gam = rho*norm(Vinf)*gam;
+lift_nondim_gam = lift_gam/(0.5*rho*norm(Vinf)^2*chordMean*CLslope*theta);
+cl_gam = lift_gam/(0.5*rho*(norm(Vinf))^2*span*chordMean);
+
+% ==================================================================
 
 % Calculate alpha
 alpha = theta-atan(abs(indVel)/norm(Vinf));
 
 % Lift from alpha
-cl_alpha = 2*pi*alpha;
-lift_alpha = cl_alpha.*(0.5*rho*(norm(Vinf))^2*dy*chord_mean);
+cl_alpha = CLslope*alpha;
+lift_alpha = cl_alpha.*(0.5*rho*(norm(Vinf))^2*dy*chordMean);
 
-% plot(y,lift_gam,'bo-')
-% hold on;
-% plot(y,lift_alpha,'ro-')
-% legend('gam','alpha')
-% grid on
+% Theoretical
+liftNonDimTheoretical = getLiftNonDimTheoretical(AR,CLslope,semiSpan,y);
 
-plot(yElem,abs(indVel),'ro-')
-grid on
+plotOpt = 3;
+switch plotOpt
+  case 1
+    plot(y,lift_nondim_gam,'bo');
+    hold on;
+    plot(y,liftNonDimTheoretical,'r-');
+    grid on
+    legend('numerical - Hshoe','Prandtl')
+    title(['Aspect ratio   ' num2str(AR)])
+    xlabel('spanwise stations')
+    ylabel('Non-dim lift')
+    
+  case 2
+    plot(y,gam,'bo');
+    grid on
+    title(['Aspect ratio   ' num2str(AR)])
+    xlabel('spanwise stations')
+    ylabel('Gam')
+    
+  case 3
+    plot(y,abs(indVel))
+    grid on
+    title(['Aspect ratio   ' num2str(AR)])
+    xlabel('spanwise stations')
+    ylabel('Vi')
+end
--- vortxl.m
+++ vortxl.m
@@ -1,8 +1,14 @@
 function vel = vortxl(P,A,B,gam)
-r0 = B-A;
+% r0 = B-A;
 r1 = P-A;
 r2 = P-B;
 
-vel = gam*cross(r1,r2)/(4*pi*(norm(cross(r1,r2)))^2);
-vel = vel*dot(r0,r1/norm(r1)-r2/norm(r2));
+% vel = gam*cross(r1,r2)/(4*pi*(norm(cross(r1,r2)))^2);
+% vel = vel*dot(r0,r1/norm(r1)-r2/norm(r2));
+
+r1_abs = norm(r1);
+r2_abs = norm(r2);
+
+vel = (r1_abs+r2_abs)*cross(r1,r2)/(r1_abs*r2_abs*(r1_abs*r2_abs+dot(r1,r2)));
+vel = gam/(4*pi)*vel;
 return;
